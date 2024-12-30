e062028ff8e64f98ce7ffe71e7e30a33afaa7013
mebernardino <mebernardino2008@gmail.com>
2024-12-29 21:53:21 -0600

    added xbox controller input

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -6,11 +6,17 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.XboxController;
 
 /** An example command that uses an example subsystem. */
 public class DriveCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final DriveTrain m_subsystem;
+  private final DriveTrain driveTrain;
+  private XboxController m_controller;
+
+  // Throttle and turn variables
+  private double throttle;
+  private double turn;
 
   /**
    * Creates a new ExampleCommand.
@@ -18,18 +24,38 @@ public class DriveCommand extends Command {
    * @param subsystem The subsystem used by this command.
    */
   public DriveCommand(DriveTrain subsystem) {
-    m_subsystem = subsystem;
+    driveTrain = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(driveTrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    // Initialize the XboxController
+    m_controller = new XboxController(0); // Replace '0' with the appropriate port for your controller
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    if(m_controller.getLeftY() > Math.abs(0.05)) { // Checks if left joystick Y is greater than deadzone
+      throttle = m_controller.getLeftY();
+    }
+    else {
+      throttle = 0;
+    }
+    if(m_controller.getRightX() > Math.abs(0.05)) { // Checks if right joystick X is greater than deadzone
+      turn = m_controller.getRightX();
+    }
+    else {
+      turn = 0;
+    }
+
+    // Drive the robot with the current throttle and turn values
+    driveTrain.manualDrive(throttle, turn);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
